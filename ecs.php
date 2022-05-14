082ad704a1ef7e5b432dde6781f36d15b726042c
Zing <zingimmick@outlook.com>
2022-05-14 22:09:20 +0800

    Use ESCConfig instead of parameters

--- ecs.php
+++ ecs.php
@@ -8,15 +8,13 @@ use Zing\CodingStandard\Set\ECSSetList;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([ECSSetList::PHP_72, ECSSetList::CUSTOM]);
-    $parameters = $ecsConfig->parameters();
     $ecsConfig->parallel();
     $ecsConfig->skip([
         \PHP_CodeSniffer\Standards\PSR1\Sniffs\Classes\ClassDeclarationSniff::class => ['*/migrations/*'],
         // Will be removed in a future major version.
         \SlevomatCodingStandard\Sniffs\TypeHints\ReturnTypeHintSniff::class,
     ]);
-    $parameters->set(
-        Option::PATHS,
+    $ecsConfig->paths(
         [__DIR__ . '/packages', __DIR__ . '/ecs.php', __DIR__ . '/monorepo-builder.php', __DIR__ . '/rector.php']
     );
 };
