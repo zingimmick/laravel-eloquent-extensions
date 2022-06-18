b1a0355465cb398f9f855cbd2bdd992473f7b75f
zingimmick <zingimmick@outlook.com>
2022-06-18 14:07:30 +0800

    Convert double quotes to single quotes for simple strings

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -5,20 +5,19 @@ declare(strict_types=1);
 use Symplify\MonorepoBuilder\Config\MBConfig;
 
 return static function (MBConfig $config): void {
-
     $config->packageDirectories([__DIR__ . '/packages']);
 
     $config->dataToAppend(
         [
-            "support" => [
-                "issues" => "https://github.com/zingimmick/laravel-eloquent-extensions/issues",
-                "source" => "https://github.com/zingimmick/laravel-eloquent-extensions",
+            'support' => [
+                'issues' => 'https://github.com/zingimmick/laravel-eloquent-extensions/issues',
+                'source' => 'https://github.com/zingimmick/laravel-eloquent-extensions',
             ],
             'authors' => [
                 [
                     'name' => 'zingimmick',
                     'email' => 'zingimmick@outlook.com',
-                    "homepage" => "https://github.com/zingimmick",
+                    'homepage' => 'https://github.com/zingimmick',
                 ],
             ],
             'require' => [
